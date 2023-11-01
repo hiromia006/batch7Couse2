e96ce1d6c8a1e78af8d53bc81ba27ed3e2189577
Hiro Mia <hiromia006@gmail.com>
2023-11-01 22:21:11 +0600

    first commit

---
+++ src/javaClassess/class1/DataTypeTest.java
@@ -0,0 +1,20 @@
+package javaClassess.class1;
+
+public class DataTypeTest {
+    public static void main(String[] args) {
+        int number = 80;
+        long world_length = 78945612312l;
+        double pi = 3.14;
+        float amount = 53.25f;
+        boolean has_id_card = true;
+        char first_letter = 'A';
+
+        System.out.println("int = " + number);
+        System.out.println("long = " + world_length);
+        System.out.println("double = " + pi);
+        System.out.println("float = " + amount);
+        System.out.println("boolean = " + has_id_card);
+        System.out.println("char = " + first_letter);
+
+    }
+}
---
+++ src/javaClassess/class1/IdentifierTest.java
@@ -0,0 +1,18 @@
+package javaClassess.class1;
+
+public class IdentifierTest {
+    public static void main(String[] args) {
+        int age = 23;
+        int Age = 23;
+        String first_name = "Leemon";
+        int _currency=50;
+        boolean b=true;
+
+        System.out.println(first_name + " age is " + age);
+
+    }
+
+    public void printSomeThing() {
+        System.out.println("Some Things");
+    }
+}
