20ce70a131513570a7eff297f4a63de30799b9f7
Hiro Mia <hiromia006@gmail.com>
2023-11-04 21:33:16 +0600

    gitHub branch concept

--- src/github/WasidTest.java
+++ src/github/WasidTest.java
@@ -4,7 +4,7 @@ public class WasidTest {
     public static void main(String[] args) {
         System.out.println("Welcome Wasid vi");
         for (int i = 0; i <= 10; i++) {
-            System.out.println("Welcome Wasid vi");
+            System.out.println("Welcome Wasid vi " + i);
         }
     }
 }
